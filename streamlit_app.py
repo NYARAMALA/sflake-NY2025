1c9399f9c09df61fbdf598f86fad08a546829199
NYARAMALA <ynaveenreddy1245@gmail.com>
2025-06-20 14:30:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,7 @@ if ingredients_list:
         st.success('Your Smoothie is ordered,'+ name_on_order+ '!',icon="✅")
 
 
-
+#New Section to display smoothiefroot nutirient information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
